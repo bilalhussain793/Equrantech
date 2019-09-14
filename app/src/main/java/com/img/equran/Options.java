ec8d1d79a120ba084e2d7e5f4d4ddea0e90901ae
Bilal Hussain <bilalhussain786b@gmail.com>
2019-09-14 22:39:48 +0500

    10:39

--- app/src/main/java/com/img/equran/Options.java
+++ app/src/main/java/com/img/equran/Options.java
@@ -8,18 +8,101 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 
 public class Options extends Fragment {
-View view;
+
+LinearLayout subscribe,refral,freemins,history,help,rate;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
-        view= inflater.inflate(R.layout.fragment_options, container, false);
+        View view= inflater.inflate(R.layout.fragment_options, container, false);
+        subscribe=view.findViewById(R.id.ln_subscribe);
+        refral=view.findViewById(R.id.ln_refral);
+        freemins=view.findViewById(R.id.ln_freemin);
+        history=view.findViewById(R.id.ln_history);
+       help =view.findViewById(R.id.ln_help);
+        rate=view.findViewById(R.id.ln_rate);
 
 
- return view;
+        subscribe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Subscribe", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        refral.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Refral", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        freemins.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "FreeMin", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "History", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Help", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        rate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "RateApp", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+
+        return view;
     }
+
 }
